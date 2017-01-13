745b9fbb11becb0a6305bc8152c69a76f9831678
Jordan Bautista <jordanbau@gmail.com>
2017-01-13 07:24:58 -0800

    Solution for Seek and Destroy

---
+++ SeekDestroy.js
@@ -0,0 +1,11 @@
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0,1);
+  
+  return arr.filter(function(val){
+    return args.indexOf(val) === -1;
+  });
+
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
