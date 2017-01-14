6d0eb31170781992a2b8308e0c494b69876a18d2
Jordan Bautista <jordanbau@gmail.com>
2017-01-13 16:24:03 -0800

    Solution for Sum All Numbers in a Range

---
+++ SumInRange.js
@@ -0,0 +1,16 @@
+function sumAll(arr) {
+  var sum = 0;
+  
+  arr.sort(function(a,b){
+    return a-b;
+  });
+  
+  for(i=arr[0]; i<=arr[1]; i++){
+    sum += i;
+  }
+  
+  
+  return sum;
+}
+
+sumAll([1, 4]);
